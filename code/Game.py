375443fc473430fbbbf81686f1d93639c05f4a35
Leonardo Paes <l.mpaes@outlook.com>
2025-03-23 14:03:08 -0300

    Adding assets

--- code/Game.py
+++ code/Game.py
@@ -2,15 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Consty import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        pygame.mixer_music.load('./asset/audio_menu.wav')
+        pygame.mixer_music.play(-1)
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
