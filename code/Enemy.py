e5b860dfeb022359e6dbafe2fcf147564fe978d8
Leonardo Paes <l.mpaes@outlook.com>
2025-03-24 22:51:05 -0300

    Creating players and enemy's
    Moving enemy's and players

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.Consty import ENTITY_SPEED, WIN_HEIGHT
+from code.Entity import Entity
 
-from Entity import Entity
 
-
-class Enemy(Entity): #verificar nomes Entity nessa classe
-    def __init__(self):
-        pass
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        self.rect.centery += ENTITY_SPEED[self.name]
+        if self.rect.bottom >= WIN_HEIGHT:
+            self.rect.top = 0
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+
 from code.Background import Background
-from code.Consty import WIN_HEIGHT
+from code.Consty import WIN_HEIGHT, WIN_WIDTH
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -15,3 +19,11 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (0, WIN_HEIGHT)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (WIN_WIDTH/ 2.5, 780))
+            case 'Player2':
+                return Player('Player2', (WIN_WIDTH/ 1.5, 780))
+            case 'Enemy1':
+                return Enemy('Enemy1', (random.randint(40,WIN_WIDTH - 40), + 10))
+            case 'Enemy2':
+                return Enemy('Enemy2', (random.randint(40,WIN_WIDTH - 40), 0))
